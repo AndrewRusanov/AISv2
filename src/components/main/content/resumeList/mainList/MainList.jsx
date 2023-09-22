3a249548b77cbc385a75c07ad3245d8e02f8c214
Andrey Rusanov <rusanow.andrej2018@yandex.ru>
2023-09-22 14:22:44 +0300

    feat: change array

--- src/components/main/content/resumeList/mainList/MainList.jsx
+++ src/components/main/content/resumeList/mainList/MainList.jsx
@@ -3,13 +3,24 @@ import styles from './MainLsit.module.css';
 import Resume from './__resume/Resume';
 import initialList from './InitialList';
 
-const MainLsit = () => {
-  return (
+const getContent = list => {
+  if (!(list.length === 0)) {
+    return (
+      <section className={styles.resume__wrapper}>
+        {list.map(item => {
+          return <Resume info={item} />;
+        })}
+        <div>крутилка</div>
+      </section>
+    );
+  } else {
     <section className={styles.resume__wrapper}>
-      {initialList.map(item => {
-        return <Resume info={item} />;
-      })}
-    </section>
-  );
+      <div>Список резюме пуст</div>
+    </section>;
+  }
+};
+
+const MainLsit = () => {
+  return <>{getContent(initialList)}</>;
 };
 export default MainLsit;
